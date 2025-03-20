636ed03b8c10e3866194cd3b9af1b85fedf20177
Torok Donat <td.zazsa@gmail.com>
2025-03-20 18:44:41 +0100

    svg létrehozás működik

---
+++ zaszlok_feladat.py
@@ -0,0 +1,48 @@
+#a kapott színt jelelő karaktert visszaadja színként ami megfelelő az svg-be
+def szinAtalakit(szin):
+    if szin == 'F':
+        return 'white'
+    elif szin == 'P':
+        return 'red'
+    elif szin == 'K':
+        return 'blue'
+    elif szin == 'Z':
+        return 'green'
+    elif szin == 'S':
+        return 'yellow'
+    elif szin == 'N':
+        return 'orange'
+    elif szin == 'L':
+        return 'purple'
+    elif szin == 'B':
+        return 'black'
+
+
+def main():
+    file = open("demo_zaszlo.txt", "r")
+
+    kockameret = 100 #az svg kockáinak a méretének a beállítása
+
+    #svg fájl kezdeti adatainak megadása
+    svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
+    svg += f'\n<svg width="{kockameret*(len(file.readline()))}" height="{kockameret*(len(file.readlines())+1)}" xmlns="http://www.w3.org/2000/svg">'
+
+    file.seek(0) #a stream alaphelyzetbe való állítása
+
+    y_count = 0
+    for line in file: #sor
+        x_count = 0
+        line = line.strip()
+        for i in range(len(line)): #oszlop
+            svg += f'\n<rect x="{x_count*kockameret}" y="{y_count*kockameret}" width="{kockameret}" height="{kockameret}" fill="{szinAtalakit(line[i])}"/>' #alakzat létrehozása
+            x_count+=1
+        y_count += 1
+
+    file.close() 
+    svg += '\n</svg>'
+
+    #svg file létrehozáse
+    file = open("eredmeny.svg","w",encoding="UTF-8")
+    file.write(svg)
+
+main()
